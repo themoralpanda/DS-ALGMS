13be79ca69e4e35007e523b57b47c112045d0b94
vigprive <vig.venkat@privetechnologies.com>
2021-07-18 13:09:28 +0800

    insertion sort code added

---
+++ com/dsalgms/src/InsertionSort.java
@@ -0,0 +1,20 @@
+package com.dsalgms.src;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public int[] sortIntegerArray(int[] a) {
+        for(int j=1;j<a.length;j++) {
+            for(int i=j-1,k=j;i>=0;i--,k--) {
+                if(a[k] < a[i]) {
+                    int c = a[k];
+                    a[k] = a[i];
+                    a[i] = c;
+                }
+
+            }
+        }
+        System.out.println(Arrays.toString(a));
+        return a;
+    }
+}
--- com/dsalgms/src/Main.java
+++ com/dsalgms/src/Main.java
@@ -1,7 +1,12 @@
 package com.dsalgms.src;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String args[]) {
+        InsertionSort iSort = new InsertionSort();
+        System.out.println(Arrays.toString(iSort.sortIntegerArray(new int[]{100,25,39,-75,-10,500})));
+        System.out.println(Arrays.toString(iSort.sortIntegerArray(new int[]{1,-20})));
 
     }
 }
