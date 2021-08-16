f480316351cc5e2420522e675e7e1581e008e28e
vigprive <vig.venkat@privetechnologies.com>
2021-08-16 19:11:18 +0800

    BST with tests included

--- src/main/java/com/dsalgms/BinarySearchTree.java
+++ src/main/java/com/dsalgms/BinarySearchTree.java
@@ -2,6 +2,22 @@ package com.dsalgms;
 
 import java.util.Comparator;
 
+
+/*
+    BST - A binary tree data structure in which the at point in time the data is always sorted.
+
+    insert, delete, search -> o(h) - where h = height of the tree.
+    traversal - o(n)
+
+    inOrder => { left, root, right }  => prints elements in ascending order.
+    preOrder => { root, left, right }
+    postOrder => { left, right, root }
+
+
+    traverse => {right, root, left} Printing elements in descending order
+
+
+ */
 public class BinarySearchTree<T> {
     private class Node {
         T data;
@@ -57,30 +73,34 @@ public class BinarySearchTree<T> {
 
     //2. Minimum
     public T minimum() {
-        return minimum(root);
+        Node t = minimum(root);
+        if(t == null) return null;
+        else return t.data;
     }
 
-    private T minimum(Node root) {
+    private Node minimum(Node root) {
         Node t = root;
         if(t == null) return null;
-        if(t.left == null) return t.data;
+        if(t.left == null) return t;
         while(t.left != null)
             t=t.left;
-        return t.data;
+        return t;
     }
 
     //3. Maximum
     public T maximum() {
-        return maximum(root);
+        Node t = maximum(root);
+        if(t == null) return null;
+        else return t.data;
     }
 
-    private T maximum(Node root) {
+    private Node maximum(Node root) {
         Node t = root;
         if(t == null) return null;
-        if(t.right == null) return t.data;
+        if(t.right == null) return t;
         while(t.right != null)
             t=t.right;
-        return t.data;
+        return t;
     }
 
     public T search(T data) {
@@ -114,8 +134,11 @@ public class BinarySearchTree<T> {
     public T predecessor(T data) {
         Node t = find(root, data);
         if(t == null) return null;
-        if(t.left != null)
-            return maximum(t.left);
+        if(t.left != null){
+            Node temp = maximum(t.left);
+            if(temp == null) return null;
+            else return temp.data;
+        }
         else {
             //travel back via parent pointer until a node less than data
             Node p = t;
@@ -133,8 +156,11 @@ public class BinarySearchTree<T> {
     public T successor(T data) {
         Node t = find(root, data);
         if(t == null) return null;
-        if(t.right != null)
-            return minimum(t.right);
+        if(t.right != null) {
+            Node temp = minimum(t.right);
+            if(temp == null) return null;
+            else return temp.data;
+        }
         else {
             //travel back via parent pointer until a node less than data
             Node p = t;
@@ -149,7 +175,147 @@ public class BinarySearchTree<T> {
     }
 
     //6. Delete
+    public T delete(T data) {
+        Node t = find(root, data);
+        if(t == null) return null;
+        //If its a leaf node.
+        if(t.left == null && t.right == null) {
+            if(root == t) {
+                root = null;
+                return data;
+            }
+            Node parent = t.parent;
+            if(parent.left == t) parent.left = null;
+            else parent.right = null;
+            //Delete t.
+            t.left = t.right = t.parent = null;
+            t.data = null;
+            t = null;
+            return data;
+        }
+
+        //If left subtree is null
+        if(t.left == null) {
+            Node rightChild = t.right;
+            Node parent = t.parent;
+            rightChild.parent = parent;
+            if(parent != null) {
+                if(parent.left == t) parent.left = rightChild;
+                else parent.right = rightChild;
+            } else {
+                root = rightChild;
+            }
+            t.parent = t.right = null;
+            t = null;
+            return data;
+        }
+        //If rightSubtree is null
+        if(t.right == null) {
+            Node leftChild = t.left;
+            Node parent = t.parent;
+            leftChild.parent = parent;
+            if(parent != null) {
+                if(parent.left == t) parent.left = leftChild;
+                else parent.right = leftChild;
+            } else {
+                root = leftChild;
+            }
+            t.parent = t.left = null;
+            t = null;
+            return data;
+        }
+
+        //If both left and right subtree is not null
+        Node min = minimum(t.right);
+        Node minimumParent = min.parent;
+        if(minimumParent.left == min)
+            minimumParent.left = (min.right != null) ? min.right : null;
+        else
+            minimumParent.right = (min.right != null) ?min.right : null;
+
+        min.parent = t.parent;
+        min.right = t.right;
+        if(t.right != null)
+            t.right.parent = min;
+        min.left = t.left;
+        if(t.left != null)
+            t.left.parent = min;
+
+        if(root == t) root = min;
+        else {
+            Node parent = t.parent;
+            if(parent.left == t) parent.left = min;
+            else parent.right = min;
+        }
+        t.left = t.right = t.parent = null;
+        t.data = null;
+        t = null;
+        return data;
+    }
+
     //7. InOrder Traversal
+    //Pattern => left, root, right
+    public void printInOrder() {
+        if(root == null) return;
+        System.out.println();
+        inOrderTraversal(root);
+        System.out.println();
+    }
+
+    private void inOrderTraversal(Node root) {
+        if(root.left != null)
+            inOrderTraversal(root.left);
+        System.out.print(" "+root.data);
+        if(root.right != null)
+            inOrderTraversal(root.right);
+    }
+
     //8. PreOrder Traversal
+    //Pattern => root, left, right
+    public void printPreOrder() {
+        if(root == null) return;
+        System.out.println();
+        preOrderTraversal(root);
+        System.out.println();
+    }
+
+    private void preOrderTraversal(Node root) {
+        System.out.print(" "+root.data);
+        if(root.left != null)
+            preOrderTraversal(root.left);
+        if(root.right != null)
+            preOrderTraversal(root.right);
+    }
+
     //9. PostOrder Traversal
+    //Pattern => left, right, root
+    public void printPostOrder() {
+        if(root == null) return;
+        System.out.println();
+        postOrderTraversal(root);
+        System.out.println();
+    }
+
+    private void postOrderTraversal(Node root) {
+        if(root.left != null)
+            postOrderTraversal(root.left);
+        if(root.right != null)
+            postOrderTraversal(root.right);
+        System.out.print(" "+root.data);
+    }
+
+    public void printDescendingOrder() {
+        if(root == null) return;
+        System.out.println();
+        descendingOrder(root);
+        System.out.println();
+    }
+
+    private void descendingOrder(Node root) {
+        if(root.right != null)
+            descendingOrder(root.right);
+        System.out.print(" "+root.data);
+        if(root.left != null)
+            descendingOrder(root.left);
+    }
 }
--- src/test/java/com/dsalgms/BinarySearchTreeTest.java
+++ src/test/java/com/dsalgms/BinarySearchTreeTest.java
@@ -46,6 +46,39 @@ class BinarySearchTreeTest {
         Assertions.assertEquals(10, bst.successor(8));
         Assertions.assertEquals(null, bst.successor(80));
 
+        bst.printInOrder();
+        bst.printPreOrder();
+        bst.printPostOrder();
+        bst.printDescendingOrder();
+
+
+        Assertions.assertEquals(2, bst.search(2));
+        bst.delete(2);
+        Assertions.assertEquals(null, bst.search(2));
+        Assertions.assertEquals(-1, bst.search(-1));
+        bst.delete(-1);
+        Assertions.assertEquals(null, bst.search(-1));
+        Assertions.assertEquals(10, bst.search(10));
+        bst.delete(10);
+        Assertions.assertEquals(null, bst.search(10));
+        Assertions.assertEquals(5, bst.search(5));
+        bst.delete(5);
+        Assertions.assertEquals(null, bst.search(5));
+        Assertions.assertEquals(6, bst.search(6));
+        bst.delete(6);
+        Assertions.assertEquals(null, bst.search(6));
+        Assertions.assertEquals(4, bst.search(4));
+        bst.delete(4);
+        Assertions.assertEquals(null, bst.search(4));
+        Assertions.assertEquals(7, bst.search(7));
+        bst.delete(7);
+        Assertions.assertEquals(null, bst.search(7));
+        Assertions.assertEquals(3, bst.search(3));
+        bst.delete(3);
+        Assertions.assertEquals(null, bst.search(3));
+        Assertions.assertEquals(8, bst.search(8));
+        bst.delete(8);
+        Assertions.assertEquals(null, bst.search(8));
     }
 
 }
